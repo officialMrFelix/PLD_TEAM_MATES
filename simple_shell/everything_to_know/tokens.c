388e856660f1df21426031dfc2764b82e22f5829
officialMrFelix <felixobianozie@hotmail.com>
2022-08-04 08:35:10 +0000

    updated structure: tokens.c

--- simple_shell/everything_to_know/tokens.c
+++ simple_shell/everything_to_know/tokens.c
@@ -5,20 +5,23 @@
 
 /**
  * tokens - splits a string and returns an array of each 
- * word of the string
- * @text - pointer to string
+ * token/word of the string
+ * @text - string to tokenize
+ * @delim - delimiter(s) - only first delimiter is considered
+ * @count - buffer to hold number of tokens returned by
+ * function
  *
- * Return:  array of strings
+ * Return:  array of strings/tokens
  */
 
-char **tokens(char *text)
+char **tokens(char *text, const char *delim, int *count)
 {
 
 	char **words;
-	const char *delim = " ";
 	int i;
 	int count_delim = 1;
 	
+	/*Estimate the number of tokens in view*/
 	i = 0;
 	while (text[i])
 	{
@@ -27,8 +30,10 @@ char **tokens(char *text)
 		i++;
 	}
 
+	/*Allocate memory based on tokens in view*/
 	words = malloc(sizeof(char *) * count_delim);
 
+	/*Tokenize string and store in reserved array*/
 	words[0] = strtok(text, delim);
 	
 	for (i = 1; i < count_delim; i++)
@@ -36,24 +41,23 @@ char **tokens(char *text)
 		words[i] = strtok(NULL, delim);
 	}
 
+	/*Return every update*/
+	*count = count_delim;
 	return (words);
 }
 
 int main(void)
 {
-	char text[] = "something here is";
+	char text[] = "This is the place where PLD TEAM MATES test their function";
 	char **words;
 	const char *delim = " ";
-	int i;
-	int count = 1;
+	int i = 0;
+	int count = 0;
 
-	for (i = 0; text[i] != '\0'; i++)
-	{
-		count++;
-	}
-
-	words = tokens(text);
-
+	/*Call to tokens*/
+	words = tokens(text, delim, &count);
+	
+	/*Print all tokens/words acquired*/
 	for (i = 0; i < count; i++)
 	{
 		printf("argument %i: %s\n", i, words[i]);
